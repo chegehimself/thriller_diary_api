8845e8222484b9c23404c84774fc17d018171e39
james-chege <jymeschege@gmail.com>
2018-07-24 17:45:57 +0300

    Test user login

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -15,7 +15,13 @@ class TestDiaryEntry(unittest.TestCase):
         self.client = self.app.test_client
         self.user = {"username":"superman", "email":"superman@gmail.com", "passowrd":"69mansuper"}
         self.register_route = 'api/v1/auth/signup'
+        self.login_route = 'api/v1/auth/login'
 
-    def test_registration(self):
+    def test_registration_success(self):
         req = self.client().post(self.register_route, data=self.user) # pylint: disable=unused-variable
         self.assertEqual(req.status_code, 201)
+
+    def test_login_success(self):
+        self.user_data = {"email":"superman@gmail.com", "passowrd":"69mansuper"}
+        req = self.client().post(self.login_route, data=self.user_data) # pylint: disable=unused-variable
+        self.assertEqual(req.status_code, 200)
