ef2d3c624e5089a437a14f2e4b8e300cb36e1334
james-chege <jymeschege@gmail.com>
2018-07-26 23:14:28 +0300

    Modify connection

--- app/models.py
+++ app/models.py
@@ -68,14 +68,10 @@ class User:
     """ returns available users """
     def all_the_users(self):
         """Returns all users in databse """
-        # HOSTNAME = 'localhost'
-        # USERNAME = 'postgres'
-        # PASSWORD = '2grateful'
-        # DATABASE = 'thriller'
-        HOSTNAME = 'ec2-107-22-169-45.compute-1.amazonaws.com'
-        USERNAME = 'xqvzxugpqzozsl'
-        PASSWORD = '6e44c7de8ec9eb08db8f5b58080378cd1c3c6bc4f4beec842949d915c4488494'
-        DATABASE = 'dbmjf8qhfukq3i'
+        HOSTNAME = 'localhost'
+        USERNAME = 'postgres'
+        PASSWORD = '2grateful'
+        DATABASE = 'thriller'
         db = psycopg2.connect( host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
         cur = db.cursor()
         cur.execute("SELECT id, username, email, password FROM users")
