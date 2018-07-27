2b4ec82ea1d662c9c406d30cc435d8497d848419
james-chege <jymeschege@gmail.com>
2018-07-27 18:19:34 +0300

    CHORE -  Import jwt error fixed

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 import psycopg2
 import datetime
-import jwt 
+import jwt
 import os
 from functools import wraps
 from flask import request, jsonify, current_app, session
