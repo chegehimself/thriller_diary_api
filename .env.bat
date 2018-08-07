ac309b78be9b347ceaf62894e4ec35413c8dbc04
james-chege <jymeschege@gmail.com>
2018-08-07 23:43:41 +0300

    Allow user to input a story of any length

--- .env.bat
+++ .env.bat
@@ -1,5 +1,10 @@
+REM set APP_SETTINGS=development
+REM set DATABASE=dbmjf8qhfukq3i
+REM set PASSWORD=6e44c7de8ec9eb08db8f5b58080378cd1c3c6bc4f4beec842949d915c4488494
+REM set USERNAME=xqvzxugpqzozsl
+REM set HOSTNAME=ec2-107-22-169-45.compute-1.amazonaws.com
 set APP_SETTINGS=development
-set DATABASE=tests
+set DATABASE=thriller
 set PASSWORD=2grateful
 set USERNAME=postgres
 set HOSTNAME=localhost
